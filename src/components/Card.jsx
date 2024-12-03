29b8f12dae7948dd4375d47f1e7adfb6c0d693cd
Glanisha <lizacastelinoo@gmail.com>
2024-12-03 15:59:00 +0530

    Semi Polished UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { assets } from '../assets/gemini-clone-assets/assets/assets'
+import React from "react";
+import { assets } from "../assets/gemini-clone-assets/assets/assets";
 
 const Card = (props) => {
   return (
-    <div className='h-72 bg-gray-100 rounded-lg'>
-      <p>{props.text}</p>
-      <img src={props.img} alt="" />
-    </div>
-  )
-}
+    <div className="relative h-72 bg-gray-100 rounded-xl p-6 flex flex-col justify-between">
+      <p className="text-lg font-medium text-gray-700">{props.text}</p>
 
-export default Card
+      <div className="absolute bottom-4 right-4">
+        <img
+          src={props.img}
+          alt="Card Icon"
+          className="w-10 h-10 rounded-full border-2 border-white shadow-md"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
