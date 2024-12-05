b91bcc266e6e58b64ca9ad35202fa14360d999ca
Glanisha <lizacastelinoo@gmail.com>
2024-12-05 19:50:02 +0530

    SideBar History Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/gemini-clone-assets/assets/assets";
 import { useState } from "react";
-
+import { Context } from "../context/Context";
 
 const Sidebar = () => {
   const [extended, setextended] = useState(false);
-  
+  const {onSent, prevPropmt, setRecentPrompt}=useContext(Context);
   
   return (
     <div
       className={`${
         extended ? "w-[260px]" : "w-[80px]"
-      } h-screen bg-white border-r border-gray-200 flex flex-col transition-all duration-100 font-outfit`}
+      } h-screen bg-white border-r border-gray-200 flex flex-col transition-all duration-100 font-outfit relative`}
     >
       <div className="p-4 flex flex-col ">
         <button
@@ -27,7 +27,7 @@ const Sidebar = () => {
         <button className="flex items-center bg-[#e5eaf1] rounded-lg px-3 py-2  hover:bg-[#d7e0ec] transition-colors mt-5">
           <img src={assets.plus_icon} alt="" className="w-5 h-5" />
           {extended ? (
-            <span className="text-gray-600 font-medium">New chat</span>
+            <span className="text-gray-600 font-medium ml-2">New chat</span>
           ) : null}
         </button>
       </div>
@@ -35,26 +35,25 @@ const Sidebar = () => {
             <span className="text-sm text-gray-700 px-4 ">Recents</span>
           ) : null}
 
-
-
-
-      <div className="px-4 py-3 flex-1">
-        <div className="hover:bg-[#f1f3f5]  rounded-lg p-3 flex items-center">
-          <img
-            src={assets.message_icon}
-            alt=""
-            className="w-5 h-5 mr-2 text-gray-600"
-          />
-          
-         
-          {extended ? (
-            <span className="text-sm text-gray-600 truncate">
-              What is React?...
-            </span>
-          ) : null}
-        </div>
+      <div className="flex-grow overflow-auto">
+        {extended?(prevPropmt.map((item, index)=>{
+          return(
+            <div key={index} className="px-4 flex">
+              <div className="hover:bg-[#f1f3f5] rounded-lg p-3 flex items-center">
+                <img
+                  src={assets.message_icon}
+                  alt=""
+                  className="w-5 h-5 mr-2 text-gray-600"
+                />
+                <span className="text-sm text-gray-600 truncate">
+                  {item}...
+                </span>
+              </div>
+            </div>
+          )
+        })):null}
       </div>
-
+      
       <div className="px-4 pb-4 space-y-4">
         <button className="w-full flex items-center p-2 hover:bg-gray-100 rounded-lg">
           <div className="w-8 h-8 rounded-full flex items-center justify-center mr-3 group-hover:bg-gray-200">
